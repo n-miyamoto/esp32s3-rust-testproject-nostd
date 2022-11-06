8ad99d586687a9119ac9074f5a8d826db23d63f9
n-miyamoto <scotsdowneroad@gmail.com>
2022-11-06 12:19:14 +0900

    add gpio control

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 #![no_main]
 
 extern crate alloc;
-use esp32s3_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use esp32s3_hal::{
+    clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc, IO,
+};
 use esp_backtrace as _;
+
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
@@ -43,5 +46,19 @@ fn main() -> ! {
     wdt1.disable();
 
     esp_println::println!("Hello World\n");
-    loop {}
+
+    // Set GPIO7 as an output, and set its state high initially.
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio7.into_push_pull_output();
+
+    led.set_high().unwrap();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let mut delay = Delay::new(&clocks);
+
+    loop {
+        led.toggle().unwrap();
+        delay.delay_ms(500u32);
+    }
 }
