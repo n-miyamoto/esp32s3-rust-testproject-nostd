c36492a866881958ba9f69d3daf7f033e7e2ab8c
n-miyamoto <scotsdowneroad@gmail.com>
2022-11-06 03:49:26 +0000

    read switch

--- src/main.rs
+++ src/main.rs
@@ -47,18 +47,23 @@ fn main() -> ! {
 
     esp_println::println!("Hello World\n");
 
-    // Set GPIO7 as an output, and set its state high initially.
+    // Set GPIO7 as an output, GPIO0 as input
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio7.into_push_pull_output();
+    let button = io.pins.gpio0.into_pull_up_input();
 
-    led.set_high().unwrap();
-
-    // Initialize the Delay peripheral, and use it to toggle the LED state in a
-    // loop.
-    let mut delay = Delay::new(&clocks);
-
+    let mut st = button.is_high().unwrap();
+    let mut st_next;
     loop {
-        led.toggle().unwrap();
-        delay.delay_ms(500u32);
+        st_next = button.is_high().unwrap();
+        if st != st_next {
+            led.set_high().unwrap();
+            if st_next {
+                esp_println::println!("button off\n");
+            } else {
+                esp_println::println!("button on\n");
+            }
+        }
+        st = st_next;
     }
 }
